1b94d6b280ce2b32a5cd16c8808d49ea509472c5
Eugene Lee <destinicafe@gmail.com>
2016-12-28 17:08:47 -0800

    Add files via upload

---
+++ MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  currencyConversion
+//
+//  Created by Eunice Lee on 12/23/16.
+//  Copyright (c) 2016 Eunice Lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
